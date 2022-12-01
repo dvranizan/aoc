1faf3625fd1f07423effeb993ab8e846d6cc4ef7
David Vranizan <david@figure8.engineering>
2022-12-01 13:58:35 -0800

    add solution for day 1

---
+++ 2022/1/solution.lisp
@@ -0,0 +1,18 @@
+(reduce #'max
+        (loop with elves
+              for elf in (loop with megalist
+                               for item in (with-open-file (stream "input.txt")
+                                             ;; collect into meglist from file
+                                             (loop for line = (read-line stream nil)
+                                                   while line
+                                                   collect (parse-integer line :junk-allowed t)))
+                               ;; split on the NILs
+                               if (equal item NIL)
+                                 ;; backup and collect into little list
+                                 collect (nreverse megalist) and do (setf megalist nil)
+                               else
+                                 ;; keep going
+                                 do (push item megalist))
+              ;; sum elf cals
+              while elf
+              collect (reduce '+ elf)))
